f0c793bcef0c372db0319251225e8350b07331b9
AltairCardinal <564406741@qq.com>
2026-07-02 01:48:47 +0800

    Bump desktop feature version

--- app-desktop/src/main/kotlin/mihon/desktop/AppVersion.kt
+++ app-desktop/src/main/kotlin/mihon/desktop/AppVersion.kt
@@ -2,7 +2,7 @@ package mihon.desktop
 
 object AppVersion {
     const val STAGE = 11
-    const val FEATURE = 11
+    const val FEATURE = 12
 }
 
 val APP_VERSION: String = "0.${AppVersion.STAGE}.${AppVersion.FEATURE}.${BuildInfo.GIT_HASH}"
